d543dadc742313aed2df9faafeed65954872a713
Venca321 <login@vaclavparma.cz>
2023-02-11 12:12:21 +0100

    wikipedia finder

--- Server/Modules/web_finder/web_finder.py
+++ Server/Modules/web_finder/web_finder.py
@@ -1,4 +1,7 @@
 
+import wikipediaapi, os
+from Modules.functions.functions import *
+
 class Match():
     def match(text, output=False):
         return 0
@@ -8,8 +11,24 @@ class Data():
         pass
 
 class Wiki():
-    def find():
-        pass
+    def find(text):
+        remove_from_input = data.read(f"{os.getcwd()}/Modules/web_finder/data/config.ini", "Wiki", "remove_from_input").split(", ")
+        remove_from_output = data.read(f"{os.getcwd()}/Modules/web_finder/data/config.ini", "Wiki", "remove_from_output").split(", ")
+
+        for checked_word in text.split(" "): #Pokud je nějaké slovo na 90%+ in words_to_remove, odeber ho
+            if Texts.match(remove_from_input, checked_word) > 90:
+                text = text.replace(checked_word, "").replace("  ", " ")
+        if text.startswith(" "): text = text[1:]
+
+        wiki = wikipediaapi.Wikipedia("cs") #Najít wiki stránku v češtině
+        page = wiki.page("python")
+        output = page.summary.split("\n")[0] #Vzít jen 1. odstavec
+        
+        for checked_word in output.split(" "): #Pokud je nějaké slovo na 90%+ in words_to_remove, odeber ho
+            if Texts.match(remove_from_output, checked_word) > 90:
+                output = output.replace(checked_word, "").replace("  ", " ")
+
+        return output
 
 class Google:
     def find():
